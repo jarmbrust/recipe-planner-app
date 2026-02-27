a11a670e932f300a9044ec9057b85b317439e472
James Armbrust <jarmbrust@users.noreply.github.com>
2026-02-27 11:25:32 -0500

    swapped out bun for npm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,7 @@
-import tailwindcss from "@tailwindcss/vite";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: "2026-02-27",
+  // fallback: "2025-07-15",
   devtools: { enabled: true },
   pages: true,
   modules: [
@@ -15,13 +14,14 @@ export default defineNuxtConfig({
     "@nuxt/hints",
     "@nuxt/ui",
     "@pinia/nuxt",
+    "@nuxt/fonts",
   ],
   shadcn: {
     prefix: "",
     componentDir: "./app/components/ui",
   },
-  css: ["~/assets/css/tailwind.css"],
-  vite: {
-    // plugins: [tailwindcss()],
-  },
+  css: ["~/assets/css/main.css"],
+  // vite: {
+  //   // plugins: [tailwindcss()],
+  // },
 });
